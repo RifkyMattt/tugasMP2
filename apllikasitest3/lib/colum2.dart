1ccd25c5aae76bdba7e1041b11f6289f6875c3d1
RifkyMattt <118195048+RifkyMattt@users.noreply.github.com>
2024-11-27 13:45:33 +0700

    Update colum2.dart

--- apllikasitest3/lib/colum2.dart
+++ apllikasitest3/lib/colum2.dart
@@ -1,20 +1,45 @@
+import 'package:flutter/material.dart';
 
-Center(
-  child: Container(
-    child: Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        OutlinedButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 10;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            OutlinedButton(
+              onPressed: _increment,
+              child: Icon(Icons.add),
+            ),
+            Text("$_counter"),
+            OutlinedButton(
+              onPressed: _decrement,
+              child: Icon(Icons.remove),
+            ),
+          ],
         ),
-        Text("10"),
-        OutlinedButton(
-          onPressed: () {},
-          child: Icon(Icons.remove),
-        ),
-      ],
-    ),
-  ),
-)
+      ),
+    );
+  }
+}
